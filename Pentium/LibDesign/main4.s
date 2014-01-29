f9d2d889a66470fd8fc372bb1a84ecdd60b2ca10
Roie Blacl <rblack@cosc2425.(none)>
2014-01-29 15:37:42 -0600

    updated lib design makefile

---
+++ Pentium/LibDesign/main4.s
@@ -0,0 +1,26 @@
+	.file	"main4.c"
+	.intel_syntax noprefix
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 16
+	mov	DWORD PTR [rbp-4], edi
+	mov	QWORD PTR [rbp-16], rsi
+	mov	eax, 0
+	call	asm_main
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 4.6.3-1ubuntu5) 4.6.3"
+	.section	.note.GNU-stack,"",@progbits
